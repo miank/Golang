892da043a02506268f9ff45047be73f0af5a67e0
Ankit Mishra <ankit1703@gmail.com>
2023-06-11 14:01:20 +0530

    More concepts on channel

--- Channel/main.go
+++ Channel/main.go
@@ -6,11 +6,12 @@ import (
 )
 
 func main() {
-	var ch chan int
-	fmt.Println(ch)
-
-	c := make(chan int)
+	var c chan int
 	fmt.Println(c)
+
+	// To define a channel we have to use the inbuilt function make
+	ch := make(chan int)
+	fmt.Println(ch)
 
 	// sending and receiving value from channel
 	fmt.Println("Sending value to channel")
@@ -20,6 +21,40 @@ func main() {
 	go receive(ch)
 
 	time.Sleep(time.Second * 1)
+
+	// Sending and receing value in main
+	ch1 := make(chan int)
+	fmt.Println("Sending value to channel start")
+	go send1(ch1)
+	val := <-ch1
+	fmt.Printf("Receiving value from channel finished. Value received: %d \n", val)
+
+	// Buffered channels
+
+	ch2 := make(chan int, 1)
+
+	ch2 <- 2
+	fmt.Println("Sending value to channel complete")
+
+	val2 := <-ch2
+
+	fmt.Printf("Receiving value from channel finished. Value received: %d\n", val2)
+
+	// Receive channel is blocked when channel is empty
+
+	ch3 := make(chan int, 1)
+	ch3 <- 1
+	fmt.Println("Sending to the channel is complete")
+	fmt.Println("Send channel is blocked so rest of the code will not execute")
+	val3 := <-ch3
+	fmt.Printf("Receiving value from channel is complete %d", val3)
+	//	val3 = <-ch3 // This will nlock the code
+
+	fmt.Printf("Receving value from channel finished. Value received: %d\n", val3)
+}
+
+func send1(ch chan int) {
+	ch <- 5
 }
 
 func send(ch chan int) {
