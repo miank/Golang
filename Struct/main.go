08279d6d5232e3cb6dcc2a01ac294cb4e38bf7f5
Ankit Mishra <ankit1703@gmail.com>
2023-06-19 13:58:11 +0530

    Struct Code Updated.

--- Struct/main.go
+++ Struct/main.go
@@ -9,6 +9,17 @@ type User struct {
 	Age    int
 }
 
+type person struct {
+	name string
+	age  int
+}
+
+func newPerson(name string) *person {
+	p := person{name: name}
+	p.age = 42
+	return &p
+}
+
 func main() {
 	fmt.Println("Structs in Golang")
 	user := User{
@@ -18,6 +29,22 @@ func main() {
 		Age:    22,
 	}
 	fmt.Printf("User Details are %+v: \n", user)
-	fmt.Printf("User Name is %v, Email is %v and age is %v: ", user.Name, user.Email, user.Age)
+	fmt.Printf("User Name is %v, Email is %v and age is %v: \n", user.Name, user.Email, user.Age)
 
+	fmt.Println(&person{
+		name: "Fred",
+	})
+
+	fmt.Println(newPerson("John"))
+
+	s := person{
+		name: "Sean",
+		age:  50,
+	}
+	fmt.Println(s)
+
+	sp := s
+	sp.age = 22
+	fmt.Println(s.age)
+	
 }
