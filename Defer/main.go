a7983a6a99a5d77880f38ad9006d9f4d49b93b4b
Ankit Mishra <ankit1703@gmail.com>
2023-06-25 12:20:32 +0530

    Code updated.

--- Defer/main.go
+++ Defer/main.go
@@ -2,16 +2,26 @@ package main
 
 import "fmt"
 
-func mult(a, b int) int {
-	result := a * b
-	fmt.Println("Result :", result)
-	return result
-}
-func main() {
-	fmt.Println("Start")
+// defer executes when sorrounding function completes.
 
-	// Multiple defer statements executes in LIFO Order
-	defer fmt.Println("End")
-	defer mult(34, 56)
-	defer mult(10, 10)
+func main() {
+	defer fmt.Println("This is printed once the function completes")
+
+	fmt.Println("This is printed first")
+	fmt.Println("This is printed second")
+
+	// defer is scoped to the function which it is declared.
+	greet()
+	fmt.Println("This is printed after the greet is called.")
+
+	defer fmt.Println("first")
+	defer fmt.Println("second")
+	defer fmt.Println("third")
+
+	fmt.Println("starting my Function...")
+}
+
+func greet() {
+	defer fmt.Println("Printed after the first greeting")
+	fmt.Println("first greeting")
 }
