faabddde5c8edc6591d89af4880f2601945a4e45
Ankit Mishra <ankit1703@gmail.com>
2023-06-26 15:03:12 +0530

    Code updated.

--- Maps/main.go
+++ Maps/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 func main() {
 	empSalary := map[string]int{}
@@ -44,4 +47,50 @@ func main() {
 		"a": 1,
 	}
 	fmt.Println(m2)
+
+	// Define a map
+
+	sample1 := map[string]string{
+		"a": "x",
+		"b": "y",
+	}
+
+	fmt.Println("Map ", sample1)
+
+	// iterate through the map
+	for x, value := range sample1 {
+		fmt.Printf("The value of %s is %s \n", x, value)
+	}
+
+	for _, v := range sample1 {
+		fmt.Printf("value : %s \n", v)
+	}
+
+	// length of a map
+
+	employeeSalary := make(map[string]int)
+	employeeSalary["Tom"] = 2000
+	employeeSalary["Sam"] = 1200
+
+	lenOfMap1 := len(employeeSalary)
+	fmt.Println("Length of the map is ", lenOfMap1)
+
+	// Check if key exist or not
+	val, ok := employeeSalary["Tom"]
+	fmt.Printf("Val: %d, ok: %t\n", val, ok)
+
+	val, ok = employeeSalary["Sam1"]
+	fmt.Printf("Val: %d, ok: %t\n", val, ok)
+
+	// json to map
+	a := make(map[int]string)
+
+	a[1] = "John"
+
+	j, err := json.Marshal(a)
+	if err != nil {
+		fmt.Printf("Error : %s", err.Error())
+	} else {
+		fmt.Println("Map to Json ", string(j))
+	}
 }
