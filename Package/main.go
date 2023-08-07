6cd856ac2806414c8eddb08ea8575dff9ff001c1
Ankit Mishra <ankit1703@gmail.com>
2023-08-07 18:32:52 +0530

    Added struct in Package

--- Package/main.go
+++ Package/main.go
@@ -1,7 +1,14 @@
 package main
 
-import hello "hello/PackageImport"
+import (
+	"fmt"
+	hello "hello/PackageImport"
+)
 
 func main() {
+	p := hello.Person{
+		Name: "John",
+	}
+	fmt.Println("Accessing struct from another package - Person Name ->", p.Name)
 	hello.SayHello()
 }
--- PackageImport/hello.go
+++ PackageImport/hello.go
@@ -2,6 +2,10 @@ package hello
 
 import "fmt"
 
+type Person struct {
+	Name string
+}
+
 func SayHello() {
 	fmt.Println("Hello from anothe package")
 }
