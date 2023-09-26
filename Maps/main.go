b98e5966c3a845ba359a263c008b9ea962b1919d
Ankit Mishra <ankit1703@gmail.com>
2023-09-26 13:55:50 +0530

    Added new code for Map

--- Maps/main.go
+++ Maps/main.go
@@ -1,109 +1,52 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	empSalary := map[string]int{}
-	fmt.Println(empSalary)
 
-	empSalary["Tom"] = 20000
-	empSalary = map[string]int{
-		"John": 1000,
-		"Sam":  1200,
-	}
-
-	fmt.Println(empSalary)
-
-	salary := empSalary["Tom"]
-	fmt.Printf("Salary: %d", salary)
-
-	// using make
-	emp1 := make(map[string]int)
-
-	// Adding a key value
-	emp1["Test"] = 2000
-	fmt.Println(emp1)
-
-	// Delete the key
-	fmt.Println(emp1)
-
-	// Length of map
-	lenOfMap := len(emp1)
-	fmt.Println("Length of map", lenOfMap)
-
+	// creating a map
 	sample := map[string]string{
 		"a": "x",
 		"b": "y",
 	}
 
+	fmt.Println(sample)
 	for k, v := range sample {
-		fmt.Printf("Key :%s value : %s \n", k, v)
+		fmt.Printf("Key : %s value : %s\n", k, v)
 	}
 
-	m2 := map[string]int{
-		"a": 1,
-	}
-	fmt.Println(m2)
-
-	// Define a map
-
-	sample1 := map[string]string{
-		"a": "x",
-		"b": "y",
+	// Looping throught keys
+	for k := range sample {
+		fmt.Printf("Key :%s\n", k)
 	}
 
-	fmt.Println("Map ", sample1)
-
-	// iterate through the map
-	for x, value := range sample1 {
-		fmt.Printf("The value of %s is %s \n", x, value)
+	// Looping throught values
+	for _, v := range sample {
+		fmt.Printf("value: %s\n", v)
 	}
 
-	for _, v := range sample1 {
-		fmt.Printf("value : %s \n", v)
-	}
+	keys := getAllKeys(sample)
+	fmt.Println(keys)
 
-	// length of a map
-
+	// length of the map
 	employeeSalary := make(map[string]int)
-	employeeSalary["Tom"] = 2000
+	employeeSalary["Alex"] = 2000
 	employeeSalary["Sam"] = 1200
 
-	lenOfMap1 := len(employeeSalary)
-	fmt.Println("Length of the map is ", lenOfMap1)
-
-	// Check if key exist or not
-	val, ok := employeeSalary["Tom"]
-	fmt.Printf("Val: %d, ok: %t\n", val, ok)
-
-	val, ok = employeeSalary["Sam1"]
-	fmt.Printf("Val: %d, ok: %t\n", val, ok)
-
-	// json to map
-	a := make(map[int]string)
-
-	a[1] = "John"
-
-	j, err := json.Marshal(a)
-	if err != nil {
-		fmt.Printf("Error : %s", err.Error())
-	} else {
-		fmt.Println("Map to Json ", string(j))
+	for k1, v1 := range employeeSalary {
+		fmt.Printf("Employee Name %s and Salary is %d \n", k1, v1)
 	}
 
-	// Make a map
-	var emp map[string]int
-	emp["Tom"] = 2000
+	lenOfMap := len(employeeSalary)
+	fmt.Println("Length of map is ", lenOfMap)
 
-	empSalary2 := map[string]int{
-		"John": 1000,
-		"Sam":  2000,
+}
+
+func getAllKeys(sample map[string]string) []string {
+	var keys []string
+	for k := range sample {
+		keys = append(keys, k)
 	}
 
-	fmt.Println(empSalary2)
-
-	
+	return keys
 }
