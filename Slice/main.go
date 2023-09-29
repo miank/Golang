6179417b284a583d13a5332a9a8df39efd90fc63
Ankit Mishra <ankit1703@gmail.com>
2023-09-29 07:49:44 +0530

    Length and Capaicty of slice

--- Slice/main.go
+++ Slice/main.go
@@ -20,5 +20,27 @@ func main() {
 	}
 
 	fmt.Println("array after", darr)
+	numa := [3]int{78, 79, 80}
+
+	nums1 := numa[:] // contains all the elements of the slice
+	nums2 := numa[:]
+	fmt.Println("Array before change 1 ", nums1)
+	fmt.Println("Array before change 1 ", nums2)
+	nums1[0] = 100
+	fmt.Println("Array after modification to slice nums1", numa)
+	nums2[1] = 101
+	fmt.Println("Array after modification to slice nums2", numa)
+
+	// Length and Capacity of slice
+	fruitArray := [...]string{"apple", "orange", "grape", "mango", "water melon", "pine apple", "chikoo"}
+	fruitSlice := fruitArray[1:3]
+	fmt.Printf("length of slice is %d and capacity is %d \n", len(fruitSlice), cap(fruitSlice))
+	fruitSlice = fruitSlice[:cap(fruitSlice)]
+	fmt.Println("After re-slicing length is ", len(fruitSlice),
+		"and capacity is ", cap(fruitSlice))
+
+	// Creating a slice using make
+	l := make([]int, 5, 5)
+	fmt.Println(l)
 
 }
