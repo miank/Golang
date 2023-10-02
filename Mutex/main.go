f35a1a556ca8c2076996097a6640b6fabb8a7ab8
Ankit Mishra <ankit1703@gmail.com>
2023-10-02 15:07:26 +0530

    Code updated for Mutex.

--- Mutex/main.go
+++ Mutex/main.go
@@ -5,26 +5,27 @@ import (
 	"sync"
 )
 
-var val = 0
+var counter int
+var mu sync.Mutex
 
-func worker(wg *sync.WaitGroup, m *sync.Mutex) {
-	m.Lock()
-	val = val + 1
-	m.Unlock()
-
-	// Wait Group will be done
-	wg.Done()
+func increment() {
+	mu.Lock()
+	defer mu.Unlock()
+	counter++
 }
 
 func main() {
 	var wg sync.WaitGroup
-	var m sync.Mutex
 
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 5; i++ {
 		wg.Add(1)
-		go worker(&wg, &m)
+		go func() {
+			defer wg.Done()
+			increment()
+		}()
 	}
 
 	wg.Wait()
-	fmt.Println("Value of x is ", val)
+	fmt.Println("Final Counter:", counter)
+
 }
