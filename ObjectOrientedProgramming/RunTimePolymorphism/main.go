3e9a8aee60c619171619780a00b65afaaa9ea49b
Ankit Mishra <ankit1703@gmail.com>
2023-10-05 09:23:39 +0530

    Polymorphism code updated.

--- ObjectOrientedProgramming/RunTimePolymorphism/main.go
+++ ObjectOrientedProgramming/RunTimePolymorphism/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 type taxSystem interface {
-	calculateTax()
+	calculateTax() int
 }
 
 type indianTax struct {
@@ -11,43 +11,58 @@ type indianTax struct {
 	income        int
 }
 
-func (i *indianTax) calculateTax() int {
-	tax := i.income * i.taxPercentage / 100
-	return tax
-}
-
 type singaporeTax struct {
 	taxPercentage int
 	income        int
 }
 
-func (i *singaporeTax) calculateTax() int {
+type usaTax struct {
+	taxPercentage int
+	income        int
+}
+
+func (s *singaporeTax) calculateTax() int {
+	tax := s.income * s.taxPercentage / 100
+	return tax
+}
+
+func (i *indianTax) calculateTax() int {
 	tax := i.income * i.taxPercentage / 100
 	return tax
 }
-func main() {
 
-	indianTax := indianTax{
+func (i *usaTax) calculateTax() int {
+	tax := i.income * i.taxPercentage / 100
+	return tax
+}
+
+func main() {
+	indianTax := &indianTax{
 		taxPercentage: 30,
 		income:        1000,
 	}
-
-	totalIndiaTax := indianTax.calculateTax()
-	fmt.Println(totalIndiaTax)
 
 	singaporeTax := &singaporeTax{
 		taxPercentage: 10,
 		income:        2000,
 	}
 
-	totalSingaporeTax := singaporeTax.calculateTax()
-	fmt.Println(totalSingaporeTax)
+	usaTax := &usaTax{
+		taxPercentage: 40,
+		income:        500,
+	}
+
+	taxSystem := []taxSystem{indianTax, singaporeTax, usaTax}
+	totalTax := calculateTotalTax(taxSystem)
+
+	fmt.Printf("Total tax is %d\n", totalTax)
+
 }
 
-// func calculateTotalTax(taxSystems []taxSystem) int {
-// 	totalTax := 0
-// 	for _, t := range taxSystems {
-// 		totalTax += t.calculateTax() //This is where runtime polymorphism happens
-// 	}
-// 	return totalTax
-// }
+func calculateTotalTax(taxSystem []taxSystem) int {
+	totalTax := 0
+	for _, t := range taxSystem {
+		totalTax += t.calculateTax()
+	}
+	return totalTax
+}
