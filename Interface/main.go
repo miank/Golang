8d1de8aa4892b37653cc2b743000498504f467b5
Ankit Mishra <ankit1703@gmail.com>
2023-10-15 16:20:53 +0530

    added non struct type and one more interface.

--- Interface/main.go
+++ Interface/main.go
@@ -6,6 +6,9 @@ type animal interface {
 	walk()
 	breathe()
 }
+type mammal interface {
+	feed()
+}
 
 type lion struct {
 	age int
@@ -15,6 +18,9 @@ type dog struct {
 	age int
 }
 
+// Non - struct Custom Type
+type cat string
+
 func (l lion) walk() {
 	fmt.Println("Lion walk")
 }
@@ -23,25 +29,45 @@ func (l lion) breathe() {
 	fmt.Println("Lion breathes")
 }
 
-func (l dog) breathe() {
+func (l lion) feed() {
+	fmt.Println("Lion feeds young")
+}
+
+func (d *dog) breathe() {
 	fmt.Println("Dog breathes")
 }
 
-func (l dog) walk() {
+func (d *dog) walk() {
 	fmt.Println("Dog walk")
 }
 
+func (c cat) breathe() {
+	fmt.Println("Cat breathes")
+}
+
+func (c cat) walk() {
+	fmt.Println("Cat walk")
+}
+
 func main() {
-	ll := lion{
+	var a animal
+	var m mammal
+
+	l := lion{
 		24,
 	}
 
-	var a animal
-	a = ll
+	a = &l
+	a.breathe()
+	a.walk()
+	m = l
+	m.feed()
+
+	a = &dog{age: 5}
 	a.breathe()
 	a.walk()
 
-	a = dog{age: 5}
+	a = cat("smokey")
 	a.breathe()
 	a.walk()
 }
