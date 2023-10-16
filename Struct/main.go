e229dfcc897340b5a0ce49c8512349d1fa0376a9
Ankit Mishra <ankit1703@gmail.com>
2023-10-16 13:24:56 +0530

    Code Updated

--- Struct/main.go
+++ Struct/main.go
@@ -1,25 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
 
-type employee struct {
-	name   string
-	age    int
-	salary int
+type Employee struct {
+	Name   string
+	Age    int
+	Salary int
 }
 
-type point struct {
-	x float64
-	y float64
+type Employee1 struct {
+	Name    string
+	Age     int
+	Salary  int
+	Address Address
+}
+
+type Address struct {
+	city    string
+	country string
 }
 
 func main() {
-	e := employee{}
+	e := Employee{}
 	fmt.Println(e)
-	emp := employee{
-		name:   "Sam",
-		age:    31,
-		salary: 2000,
+	emp := Employee{
+		Name:   "Sam",
+		Age:    31,
+		Salary: 2000,
 	}
 
 	fmt.Printf("emp : %v\n", emp)
@@ -27,14 +38,14 @@ func main() {
 	fmt.Printf("emp : %#v\n", emp)
 	fmt.Println(emp)
 
-	n := emp.name
+	n := emp.Name
 	fmt.Printf("Current Name is  : %+v\n", n)
 
-	emp.name = "John"
-	fmt.Printf("New Name is : %s\n", emp.name)
+	emp.Name = "John"
+	fmt.Printf("New Name is : %s\n", emp.Name)
 
 	// Creating struct without field names
-	emp1 := employee{"John", 32, 4000}
+	emp1 := Employee{"John", 32, 4000}
 
 	fmt.Printf("emp : %v\n", emp1)
 	fmt.Printf("emp : %+v\n", emp1)
@@ -46,7 +57,36 @@ func main() {
 	fmt.Printf("EmpP: %v\n", empP)
 
 	// Another way of creating pointer to struct
-	empP1 := &employee{"John", 32, 4000}
+	empP1 := &Employee{"John", 32, 4000}
 	fmt.Printf("EmpP1: %v\n", empP1)
+
+	// Marshall
+	empJSON, err := json.Marshal(emp)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	fmt.Printf("MarshalIndent funnction output %s\n", string(empJSON))
+
+	// Anonymous Fields
+	type animal struct {
+		string
+		age int
+	}
+
+	a := animal{string: "Lion", age: 24}
+	n = a.string
+
+	fmt.Printf("Current name is: %s\n", n)
+	a.string = "Tiger"
+	fmt.Printf("New name is: %s\n", a.string)
+
+	// Nested Struct
+
+	addr := Address{city: "London", country: "UK"}
+
+	emp4 := Employee1{Name: "Sam", Age: 31, Salary: 6000, Address: addr}
+
+	fmt.Printf("Emp4 with address %v", emp4)
 
 }
