424cbe2678cbfc2479b4b2c00a7b362933bee441
Ankit Mishra <ankit1703@gmail.com>
2024-07-20 20:48:04 +0530

    Code updated.

--- Select/main.go
+++ Select/main.go
@@ -2,59 +2,33 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	ch1 := make(chan string)
 	ch2 := make(chan string)
-	ch3 := make(chan string)
-	ch4 := make(chan string)
 
 	go goOne(ch1)
 	go goTwo(ch2)
 
-	select {
-	case msg1 := <-ch1:
-		fmt.Println(msg1)
-	case msg2 := <-ch2:
-		fmt.Println(msg2)
-	}
-
-	// Print both channel using loop
-
-	go goThree(ch3)
-	go goFour(ch4)
-
 	for i := 0; i < 2; i++ {
+
 		select {
-		case msg3 := <-ch3:
-			fmt.Println(msg3)
-		case msg4 := <-ch4:
-			fmt.Println(msg4)
+		case msg1 := <-ch1:
+			fmt.Println(msg1)
+		case msg2 := <-ch2:
+			fmt.Println(msg2)
 		}
 	}
 }
 
 func goOne(ch chan string) {
-	//	time.Sleep(time.Millisecond * 1)
-
+	time.Sleep(time.Second * 2)
 	ch <- "From goOne goroutine"
 }
 
 func goTwo(ch chan string) {
-	// time.Sleep(time.Millisecond * 1)
-
-	ch <- "From goTwo goRoutine"
-}
-
-func goThree(ch chan string) {
-	//	time.Sleep(time.Millisecond * 1)
-
-	ch <- "From goThree goroutine"
-}
-
-func goFour(ch chan string) {
-	// time.Sleep(time.Millisecond * 1)
-
-	ch <- "From goFour goRoutine"
+	time.Sleep(time.Second * 1)
+	ch <- "From goTwo goroutine"
 }
