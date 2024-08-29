ae51a930cfcc09c60165d430c28fc1e2a84eed9d
Ankit Mishra <ankit1703@gmail.com>
2024-08-29 14:10:58 +0530

    Code Updated of Pointers.

--- Pointers/main.go
+++ Pointers/main.go
@@ -2,116 +2,24 @@ package main
 
 import "fmt"
 
-type Person struct {
-	name string
-	age  int
-}
-
-func callByValue(num int) {
-	num = 40
-	fmt.Println("Call by Value ", num)
-}
-
-func callByReference(num *int) {
-	*num = 50
-	fmt.Println("Call by reference ", *num)
-}
-
-func update(num *int) {
-	*num = 30
-}
-
-func display() *string {
-	message := "Hello World"
-	return &message
-}
-
-// Passing Pointers
-
-func ptf(a *int) {
-	*a = 74
-	fmt.Printf("The value of parameter changed %d \n", *a)
-}
-
 func main() {
-	var num int = 5
+	var username string = "Go Developer"
+	var usernamePointer *string = &username
 
-	// Prints the value stored in variable
-	fmt.Println("Variable Value: ", num)
+	fmt.Println(*usernamePointer)
+	fmt.Println(&usernamePointer)
 
-	// Prints the address of variable
-	fmt.Println("Memory Address: ", &num)
+	var age int = 12
+	var isAwesome = true
+	var height = 5.5
 
-	var ptr *int = &num
-	fmt.Println("Pointer Value: ", *ptr)
-	fmt.Println("Pointer Address: ", ptr)
+	// Declaring pointer of type
 
-	var name = "John"
-	var ptr1 *string
-	ptr1 = &name
+	var isAwesomePointer *bool = &isAwesome
+	var heightPointer *float64 = &height
+	var agePointer *int = &age
 
-	// Prints the value stored in variable
-	fmt.Println("Variable Value: ", name)
-
-	// Prints the address of variable
-	fmt.Println("Memory Address: ", &ptr1)
-	// Pointer Value
-	fmt.Println("Pointer Value: ", *ptr1)
-
-	var ptr3 *int
-	ptr3 = &num
-	num = 6
-	*ptr3 = 7
-
-	fmt.Println("Value of num ", num)
-	fmt.Println("Value of ptr3 ", *ptr3)
-
-	// Pointer and functions
-
-	fmt.Println("The current value of num is ", num)
-
-	update(&num)
-
-	fmt.Println("After update pointer number is ", num)
-
-	// Return a pointer from function
-
-	result := display()
-	fmt.Println("The return value from function via pointer -->>", *result)
-
-	// Call By Value
-	callByValue(num)
-
-	// Call By Reference
-	callByReference(&num)
-
-	person1 := Person{"John", 25}
-
-	var ptr2 *Person // Create a pointer of type person
-	ptr2 = &person1  // store the address of person
-
-	fmt.Println("Person struct ", person1)
-
-	fmt.Println("Person struct pointer ", ptr2)
-
-	// access the name member
-	fmt.Println("Name:", ptr2.name)
-
-	// access the age member
-	fmt.Println("Age:", ptr2.age)
-
-	// Default Value of a Pointer
-	var a *int
-	fmt.Println("Default Value of a pointer", a)
-
-	// Pointer Arithmetic
-	fmt.Println("Pointer Arithmetic is not achievable in golang")
-	a1 := 1
-	b := &a1
-	// b = b + 1
-	fmt.Println("b = b + 1 (Pointer Arithmetic) is not possible in Golang", *b, &b)
-
-	var x = 100
-	fmt.Println("The value of x before sending ", x)
-	ptf(&x)
+	fmt.Printf("agePointer: %v  ===> age value: %v \n", agePointer, *agePointer)
+	fmt.Printf("isAwesomePointer: %v  ===> isAwesome value: %v \n", isAwesomePointer, *isAwesomePointer)
+	fmt.Printf("heightPointer : %v  ===> height value: %v \n", heightPointer, *heightPointer)
 }
