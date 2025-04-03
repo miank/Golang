7740f34eb40f657d880e55ea959aba4a1d941114
Ankit Mishra <ankit1703@gmail.com>
2025-04-03 15:22:43 +0530

    Singleton pattern

--- DesignPattern/SingletonPattern/main.go
+++ DesignPattern/SingletonPattern/main.go
@@ -6,24 +6,54 @@ import (
 )
 
 var lock = &sync.Mutex{}
+var once sync.Once
 
 type single struct {
 }
 
-var singleInstance *single
+var (
+	singleInstance,
+	singleInstance_1 *single
+)
 
 func getInstance() *single {
 	if singleInstance == nil {
 		lock.Lock()
 		defer lock.Unlock()
 		if singleInstance == nil {
-			fmt.Println("Creting Single Instance Now")
+			fmt.Println("Creating single instance now.")
 			singleInstance = &single{}
 		} else {
-			fmt.Println("Single Instance already created-1")
+			fmt.Println("Single instance already created 1.")
 		}
 	} else {
-		fmt.Println("Single Instance already created-2")
+		fmt.Println("Single instance already created 2.")
 	}
+
 	return singleInstance
+}
+
+func getInstance_1() *single {
+	if singleInstance_1 == nil {
+		once.Do(
+			func() {
+				fmt.Println("Creating single instance now with Once")
+				singleInstance_1 = &single{}
+			})
+	} else {
+		fmt.Println("Single instance already created.")
+	}
+
+	return singleInstance_1
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		go getInstance()
+	}
+
+	for i := 0; i < 10; i++ {
+		go getInstance_1()
+	}
+	fmt.Scanln()
 }
