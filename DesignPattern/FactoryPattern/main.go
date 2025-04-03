c0164bc5b3b410e2b94be8eaf35615668aed565c
Ankit Mishra <ankit1703@gmail.com>
2025-04-03 17:50:01 +0530

    Code updated.

--- DesignPattern/FactoryPattern/main.go
+++ DesignPattern/FactoryPattern/main.go
@@ -6,38 +6,62 @@ import "fmt"
 
 type Vechile interface {
 	Drive() string
+	GetInfo() string
 }
 
-type car struct{}
-
-func (c car) Drive() string {
-	return "Driving a car !!"
+type Car struct {
+	Brand string
 }
 
-type bike struct{}
-
-func (b bike) Drive() string {
-	return "Driving a bike"
+func (c Car) Drive() string {
+	return fmt.Sprintf("🚗 %s car is driving!", c.Brand)
 }
 
-func NewVehicle(vechileType string) Vechile {
+func (c Car) GetInfo() string {
+	return fmt.Sprintf("Car Brand: %s", c.Brand)
+}
+
+type Bike struct {
+	Model string
+}
+
+func (b Bike) Drive() string {
+	return fmt.Sprintf("🏍️ %s bike is riding!", b.Model)
+}
+
+func (b Bike) GetInfo() string {
+	return fmt.Sprintf("Car Brand: %s", b.Model)
+}
+
+func NewVehicle(vechileType, details string) Vechile {
 	if vechileType == "car" {
-		return car{}
+		return Car{Brand: details}
 	} else if vechileType == "bike" {
-		return bike{}
+		return Bike{Model: details}
 	}
 	return nil
 }
 
 func main() {
-	car := NewVehicle("car")
-	fmt.Println(car.Drive())
+	car := NewVehicle("car", "Tesla")
+	if car != nil {
+		fmt.Println(car.Drive())   // 🚗 Tesla car is driving!
+		fmt.Println(car.GetInfo()) // Car Brand: Tesla
+	} else {
+		fmt.Println("Invalid vehicle type")
+	}
 
-	bike := NewVehicle("bike")
-	fmt.Println(bike.Drive())
+	// Create a Bike with a model
+	bike := NewVehicle("bike", "Yamaha")
+	if bike != nil {
+		fmt.Println(bike.Drive())   // 🏍️ Yamaha bike is riding!
+		fmt.Println(bike.GetInfo()) // Bike Model: Yamaha
+	} else {
+		fmt.Println("Invalid vehicle type")
+	}
 
 	// Handling an unknown vehicle type
-	unknown := NewVehicle("truck")
+	unknown := NewVehicle("truck", "Ashok")
 	if unknown == nil {
 		fmt.Println("Unknown vehicle type!")
 	}
