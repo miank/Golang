e0a5f4943dc4c294ce1e6816a9d843b24b204632
Ankit Mishra <ankit1703@gmail.com>
2025-04-21 14:36:03 +0530

    Code updated.

--- Select/main.go
+++ Select/main.go
@@ -9,26 +9,22 @@ func main() {
 	ch1 := make(chan string)
 	ch2 := make(chan string)
 
-	go goOne(ch1)
-	go goTwo(ch2)
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch1 <- "Hello from Channel1"
+	}()
 
-	for i := 0; i < 2; i++ {
+	go func() {
+		time.Sleep(1 * time.Second)
+		ch2 <- "Hello from Channel2"
+	}()
 
-		select {
-		case msg1 := <-ch1:
-			fmt.Println(msg1)
-		case msg2 := <-ch2:
-			fmt.Println(msg2)
-		}
+	select {
+	case msg1 := <-ch1:
+		fmt.Println("Received:", msg1)
+	case msg2 := <-ch2:
+		fmt.Println("Received:", msg2)
+	default:
+		fmt.Println("No Channel is ready yet")
 	}
-}
-
-func goOne(ch chan string) {
-	time.Sleep(time.Second * 2)
-	ch <- "From goOne goroutine"
-}
-
-func goTwo(ch chan string) {
-	time.Sleep(time.Second * 1)
-	ch <- "From goTwo goroutine"
 }
