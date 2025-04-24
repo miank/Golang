f84a07ca309423272e2334c96c3a680f2284010b
Ankit Mishra <ankit1703@gmail.com>
2025-04-24 12:43:30 +0530

    Code on Pointers with Maps and structs.

--- Pointers/main.go
+++ Pointers/main.go
@@ -2,24 +2,77 @@ package main
 
 import "fmt"
 
+type User struct {
+	Name string
+	Age  int
+}
+
+func Update(val *int) {
+	*val = *val + 10
+}
+
+func (u *User) HaveBirthday() {
+	u.Age++
+}
+
+// Value Receiver copies the struct - Pointer receiver modify the original.
+
+func (u User) ChangeName(name string) {
+	u.Name = name // won't affect original
+}
+
+func (u *User) RealChangeName(name string) {
+	u.Name = name // will change original
+}
+
+type Task struct {
+	Title string
+	Done  bool
+}
+
 func main() {
-	var username string = "Go Developer"
-	var usernamePointer *string = &username
+	var x int = 10
+	var p *int = &x
 
-	fmt.Println(*usernamePointer)
-	fmt.Println(&usernamePointer)
+	fmt.Println("Value of x is ", x)
+	fmt.Println("Value of *p is ", *p)
+	*p = 20
+	fmt.Println("Value of *p after update is ", *p)
 
-	var age int = 12
-	var isAwesome = true
-	var height = 5.5
+	x1 := 5
+	Update(&x1)
+	fmt.Println("Value of x1 is ", x1)
 
-	// Declaring pointer of type
+	u := User{Name: "Alice", Age: 25}
+	p1 := &u
 
-	var isAwesomePointer *bool = &isAwesome
-	var heightPointer *float64 = &height
-	var agePointer *int = &age
+	fmt.Println(p1.Name)
+	fmt.Println((*p1).Age)
 
-	fmt.Printf("agePointer: %v  ===> age value: %v \n", agePointer, *agePointer)
-	fmt.Printf("isAwesomePointer: %v  ===> isAwesome value: %v \n", isAwesomePointer, *isAwesomePointer)
-	fmt.Printf("heightPointer : %v  ===> height value: %v \n", heightPointer, *heightPointer)
+	u = User{
+		Name: "Bob",
+		Age:  30,
+	}
+
+	u.ChangeName("John")
+	u.RealChangeName("Alex") // This will change the name.
+
+	u.HaveBirthday()
+	fmt.Println(u.Name)
+	fmt.Println(u.Age)
+
+	tasks := []*Task{
+		{Title: "Learn Go", Done: false},
+		{Title: "Build Project", Done: false},
+	}
+
+	// Mark all the tasks as done
+	for _, t := range tasks {
+		t.Done = true
+	}
+
+	for _, t := range tasks {
+		fmt.Println(t.Title, "Done?", t.Done)
+	}
+
 }
