a73b56ac142648bcc0489879e8ab7e5736aa3f55
Ankit Mishra <ankit1703@gmail.com>
2025-04-28 14:13:23 +0530

    Worker Pool Code updated.

--- GoRoutines/WorkerPool/main.go
+++ GoRoutines/WorkerPool/main.go
@@ -1,62 +1,39 @@
-// Worker Pool
-
-// Implement a worker pool where multiple goroutines (workers) pick up tasks from a shared task queue (channel).
-
 package main
 
 import (
 	"fmt"
-	"math/rand"
-	"sync"
 	"time"
 )
 
-type Job struct {
-	ID        int
-	RandomNum int
-}
-
-type Result struct {
-	JobID  int
-	Square int
-}
-
-func worker(id int, jobs <-chan Job, result chan<- Result, wg *sync.WaitGroup) {
-
-	defer wg.Done()
+func worker(id int, jobs <-chan int, results chan<- int) {
 	for job := range jobs {
-		fmt.Printf("Worker %d processing jobs %d \n", id, job.ID)
-		time.Sleep(time.Millisecond * time.Duration(rand.Intn(500)))
-		result <- Result{
-			JobID: job.ID, Square: job.RandomNum * job.RandomNum}
+		fmt.Printf("Worker started %d job %d \n", id, job)
+		time.Sleep(time.Second)
+		fmt.Printf("Worker %d finished %d \n", id, job)
+		results <- job * 2
 	}
 }
 
 func main() {
+	const numJobs = 5
+	const numWorkers = 3
 
-	const (
-		numJobs    = 10
-		numWorkers = 3
-	)
+	jobs := make(chan int, numJobs)
+	results := make(chan int, numJobs) // no of jobs that many results
 
-	jobs := make(chan Job, numJobs)
-	results := make(chan Result, numJobs)
-
-	var wg sync.WaitGroup
-
-	for i := 1; i <= numWorkers; i++ {
-		wg.Add(1)
-		go worker(i, jobs, results, &wg)
+	// First start the workers and the send then assign the jobs
+	for j := 1; j <= numWorkers; j++ {
+		go worker(j, jobs, results)
 	}
 
-	// send jobs
-	for j := 1; j <= numJobs; j++ {
-		jobs <- Job{ID: j, RandomNum: rand.Intn(100)}
+	for j := 1; j <= 10; j++ {
+		jobs <- j
 	}
 
-	// collect result
-	for result := range results {
-		fmt.Printf("Result: JobID=%d, Square=%d\n", result.JobID, result.Square)
-	}
+	close(jobs)
 
+	// Collect results
+	for r := 1; r <= numJobs; r++ {
+		fmt.Println("Result:", <-results)
+	}
 }
