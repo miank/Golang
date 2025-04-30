255c908b73935ccdc6d58ec5c8a47220ac80ffc8
Ankit Mishra <ankit1703@gmail.com>
2025-04-30 17:01:23 +0530

    Select Code updated.

--- Select/main.go
+++ Select/main.go
@@ -2,37 +2,29 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
-// If you must use default, only do it for non-blocking checks.
-// Otherwise, it’ll skip even if channels aren’t ready.
-
 func main() {
-	var wg sync.WaitGroup
 	ch1 := make(chan string)
 	ch2 := make(chan string)
 
-	wg.Add(2)
 	go func() {
-		defer wg.Done()
-		time.Sleep(2 * time.Second)
-		ch1 <- "Hello from Channel1"
-	}()
-
-	go func() {
-		defer wg.Done()
 		time.Sleep(1 * time.Second)
-		ch2 <- "Hello from Channel2"
+		ch1 <- "from ch1"
 	}()
 
-	select {
-	case msg1 := <-ch1:
-		fmt.Println("Received:", msg1)
-	case msg2 := <-ch2:
-		fmt.Println("Received:", msg2)
-	}
-	wg.Wait()
+	go func() {
+		time.Sleep(1 * time.Second)
+		ch2 <- "from ch2"
+	}()
 
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-ch1:
+			fmt.Println("Received:", msg1)
+		case msg2 := <-ch2:
+			fmt.Println("Received:", msg2)
+		}
+	}
 }
