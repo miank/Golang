7325b66d4e1a724370d177d0c691ff3bf8588565
Ankit Mishra <ankit1703@gmail.com>
2025-05-14 19:03:47 +0530

    WorkerPool

--- GoRoutines/WorkerPool/main.go
+++ GoRoutines/WorkerPool/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -21,19 +22,26 @@ func main() {
 	jobs := make(chan int, numJobs)
 	results := make(chan int, numJobs) // no of jobs that many results
 
+	var wg sync.WaitGroup
 	// First start the workers and the send then assign the jobs
 	for j := 1; j <= numWorkers; j++ {
+		wg.Add(1)
 		go worker(j, jobs, results)
 	}
 
-	for j := 1; j <= 10; j++ {
+	for j := 1; j <= numJobs; j++ {
 		jobs <- j
 	}
 
 	close(jobs)
 
+	wg.Wait()
+
+	close(results)
+
 	// Collect results
 	for r := 1; r <= numJobs; r++ {
 		fmt.Println("Result:", <-results)
 	}
+
 }
