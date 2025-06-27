932b8ab665df88d02c7af4ab61d32023888d3d83
Ankit <ankit1703@gmail.com>
2025-06-27 15:55:38 +0530

    Rewrote the code.

--- PracticeQuestions/PingPong/main.go
+++ PracticeQuestions/PingPong/main.go
@@ -7,38 +7,32 @@ import (
 
 func Ping(ch chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
-
-	for i := 0; i < 5; i++ {
-		<-ch
-		fmt.Println("*** Ping ***")
+	for i := 0; i < 10; i++ {
 		ch <- 1
+		fmt.Println("***Ping***")
+		<-ch
 	}
-
 }
 
 func Pong(ch chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
-
-	for i := 0; i < 5; i++ {
-		ch <- 1
-		fmt.Println("^^^ Pong ^^^")
+	for i := 0; i < 10; i++ {
 		<-ch
+		fmt.Println("^^^Pong^^^")
+		ch <- 1
 	}
 }
 
 func main() {
-
 	ch := make(chan int, 1)
 	var wg sync.WaitGroup
 
 	wg.Add(2)
-
 	go Ping(ch, &wg)
 	go Pong(ch, &wg)
 
-	//	ch <- 1
+	ch <- 1
 
 	wg.Wait()
 	close(ch)
-
 }
